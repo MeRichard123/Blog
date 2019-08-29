e6f0caf4ff2e8508fcf9042b2f75335ad04d8a08
Richard <51269862+MeRichard123@users.noreply.github.com>
2019-08-29 12:13:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
-  alert(
-    "If you want to send a message with the form at the bottom, just click send and don't enter details you'll be redirected"
-  );
+  //alert(
+    //"If you want to send a message with the form at the bottom, just click send and don't enter details you'll be redirected"
+  //);
   const button = document.querySelector(".submit");
   button.addEventListener("click", () => {
     console.log("button clicked");
